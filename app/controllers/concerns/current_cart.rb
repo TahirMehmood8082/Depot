8c59966a4013c0fac45f6f20ad0e1d32738b38c2
TahirMehmood8082 <infinitidev31aug22@gmail.com>
2023-01-26 18:42:00 +0500

    chapter 12

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,11 @@
+module CurrentCart
+  #private prevents Rails from ever
+  # making it available as an action on the controller.
+  private
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
+  end
+end
