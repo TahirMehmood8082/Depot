851227ffcfe97aa8b927613979db7593e52aae90
TahirMehmood8082 <infinitidev31aug22@gmail.com>
2023-01-27 17:01:24 +0500

    Action mailer functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,6 +30,10 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        #OrderMailer.received(@order).deliver_later
+        #@order.charge!(pay_type_params) # do not do this
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
+
         format.html { redirect_to store_index_url, notice:
           'Thank you for your order.' }
         format.json { render :show, status: :created,
@@ -39,6 +43,18 @@ class OrdersController < ApplicationController
         format.json { render json: @order.errors,
                              status: :unprocessable_entity }
       end
+    end
+  end
+
+  def pay_type_params
+    if order_params[:pay_type] == "Credit card"
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == "Purchase order"
+      params.require(:order).permit(:po_number)
+    else
+      {}
     end
   end
 
