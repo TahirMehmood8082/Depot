e753b9dc24dfed8ee71bb25fc1969e3d3d6b7249
TahirMehmood8082 <infinitidev31aug22@gmail.com>
2023-01-30 11:44:44 +0500

    admin users logic complete here

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   # re-write password to check you enter same, for this password field type must be digest
   has_secure_password
+
+  private
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise Error.new "Can't delete last user"
+    end
+  end
 end
