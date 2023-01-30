054aaaeafa893efffa70d274561212ab5f543c57
TahirMehmood8082 <infinitidev31aug22@gmail.com>
2023-01-30 13:19:00 +0500

    localization added to depot application

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,22 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-  get 'admin/index'
-  get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
+  resources :products do
+    get :who_bought, on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   resources :products do
     get :who_bought, on: :member
   end
-
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
